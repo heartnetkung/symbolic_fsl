ae801fadedf509416614a70afb69fed62650c3df
heartnetkung <heartnetkung@gmail.com>
2025-07-18 06:42:41 +0100

    implement fixture

---
+++ test/test_fixture_1/test_5.py
@@ -0,0 +1,33 @@
+from ..test_action.util import *
+from ...arc.expert.edit.colorize import colorize
+
+
+def get_parsed_state():
+    problem_no = 5
+    dataset = read_datasets(DatasetChoice.train_v1)[problem_no]
+    init_state = dataset.to_training_state()
+    parsing_actions = [
+        IndependentParse(x_mode=ParseMode.partition,
+                         y_mode=ParseMode.crop,
+                         x_bg_model=ConstantModel(0),
+                         y_bg_model=ConstantModel(0),
+                         unknown_background=False),
+        ReparseEdge(param=ReparseEdgeParam.skip),
+        MergeNearby(param=MergeNearbyParam.skip),
+        ReparseStack(param=ReparseStackParam.skip),
+        ReparseSplit(param=ReparseSplitParam.skip)
+    ]
+    return run_actions(init_state, parsing_actions)
+
+
+def test_basic():
+    params = GlobalParams()
+    parsed_state = get_parsed_state()
+
+    action = Intersect(feat_indexes=[0, 1], color=1)
+    program = AttentionExpertProgram(action, params)
+    result = program.run(parsed_state)
+    assert result.out_shapes is not None
+
+    for out_shapes, y_shapes in zip(result.out_shapes, result.y_shapes):
+        assert colorize(out_shapes[1], 2) == y_shapes[0]
