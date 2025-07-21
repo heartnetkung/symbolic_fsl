8ee1aa92f1098aeda3e5bc82703b00b04ce4100f
heartnetkung <heartnetkung@gmail.com>
2025-07-21 10:28:42 +0100

    change colorize conditioning

--- arc/expert/edit/colorize_expert.py
+++ arc/expert/edit/colorize_expert.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from .colorize import Colorize
 from ..util import *
+from ...manager.reparse.list_reparse_relationship import find_subshape
 
 
 class ColorizeExpert(Expert[ArcTrainingState, TrainingAttentionTask]):
@@ -14,7 +15,8 @@ class ColorizeExpert(Expert[ArcTrainingState, TrainingAttentionTask]):
 
     def solve_problem(self, state: ArcTrainingState,
                       task: TrainingAttentionTask)->list[Action]:
-        label = extract_label(get_y_shapes(state, task.atn))
+        y_shapes = get_y_shapes(state, task.atn)
+        label = extract_label(y_shapes)
         if label is None:
             return []
 
@@ -26,7 +28,7 @@ class ColorizeExpert(Expert[ArcTrainingState, TrainingAttentionTask]):
                 continue
             if np.array_equal(current_values, label):
                 continue
-            if not has_relationship(task.atn, 'same_colorless_shape', i):
+            if not _is_colorless_subshapes(x_shapes, y_shapes):
                 continue
             result.append(Colorize(MemorizedModel(label), self.params, i))
         return result
@@ -49,3 +51,18 @@ def extract_label(shapes: list[Shape])->Optional[np.ndarray]:
         else:
             raise Exception('unknown shape implementation')
     return np.array(result)
+
+
+def _is_colorless_subshapes(x_shapes: list[Shape], y_shapes: list[Shape])->bool:
+    for x_shape, y_shape in zip(x_shapes, y_shapes):
+        if not _is_colorless_subshape(x_shape, y_shape):
+            return False
+    return True
+
+
+def _is_colorless_subshape(x_shape: Shape, y_shape: Shape)->bool:
+    if 'colorless_subshape' in find_subshape(x_shape, y_shape):
+        return True
+    if 'colorless_subshape' in find_subshape(y_shape, x_shape):
+        return True
+    return False
