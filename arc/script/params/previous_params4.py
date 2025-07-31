6905334476a41d0f42c4f58831aaa7ce3e1f9a7e
heartnetkung <heartnetkung@gmail.com>
2025-07-31 11:49:24 +0100

    add more previous params

--- arc/script/params/previous_params4.py
+++ arc/script/params/previous_params4.py
@@ -30,6 +30,14 @@ previous_v1_params[325] = GlobalParams(
     parser_x_modes=(ParseMode.proximity_diag,),
     parser_y_modes=(ParseMode.proximity_diag,), max_reparse=0)
 
+previous_v1_params[336] = GlobalParams(
+    parser_x_modes=(ParseMode.color_proximity_diag,),
+    parser_y_modes=(ParseMode.color_proximity_diag,), max_reparse=0)
+
+previous_v1_params[338] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_diag,),
+    parser_y_modes=(ParseMode.proximity_diag,), max_reparse=0)
+
 previous_v1_params[351] = GlobalParams(
     parser_x_modes=(ParseMode.crop,),
     parser_y_modes=(ParseMode.crop,), max_reparse=0)
@@ -53,3 +61,19 @@ previous_v1_params[357] = GlobalParams(
 previous_v1_params[361] = GlobalParams(
     parser_x_modes=(ParseMode.partition,),
     parser_y_modes=(ParseMode.proximity_diag,), max_reparse=1)
+
+previous_v1_params[371] = GlobalParams(
+    parser_x_modes=(ParseMode.partition,),
+    parser_y_modes=(ParseMode.crop,), max_reparse=1)
+
+previous_v1_params[374] = GlobalParams(
+    parser_x_modes=(ParseMode.crop,),
+    parser_y_modes=(ParseMode.proximity_diag,), max_reparse=0)
+
+previous_v1_params[379] = GlobalParams(
+    parser_x_modes=(ParseMode.crop,),
+    parser_y_modes=(ParseMode.crop,), max_reparse=0)
+
+previous_v1_params[384] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_diag,),
+    parser_y_modes=(ParseMode.proximity_diag,), max_reparse=0)
