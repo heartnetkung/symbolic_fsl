93bdce64b4f4cda048fb5c533fbb42c07d7b6e1f
heartnetkung <heartnetkung@gmail.com>
2025-07-31 01:40:25 +0100

    add more previous params

---
+++ arc/script/params/previous_params3.py
@@ -0,0 +1,63 @@
+from ...constant import *
+
+previous_v1_params = {}
+
+previous_v1_params[209] = GlobalParams(
+    parser_x_modes=(ParseMode.crop,),
+    parser_y_modes=(ParseMode.crop,), max_reparse=0)
+
+previous_v1_params[219] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_diag,),
+    parser_y_modes=(ParseMode.color_proximity_diag,), max_reparse=0)
+
+previous_v1_params[250] = GlobalParams(
+    parser_x_modes=(ParseMode.crop,),
+    parser_y_modes=(ParseMode.crop,), max_reparse=0)
+
+previous_v1_params[251] = GlobalParams(
+    parser_x_modes=(ParseMode.crop,),
+    parser_y_modes=(ParseMode.crop,), max_reparse=0)
+
+previous_v1_params[257] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_diag,),
+    parser_y_modes=(ParseMode.color_proximity_diag,), max_reparse=0)
+
+previous_v1_params[260] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_diag,),
+    parser_y_modes=(ParseMode.proximity_diag,), max_reparse=0)
+
+previous_v1_params[261] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_normal,),
+    parser_y_modes=(ParseMode.color_proximity_diag,), max_reparse=0)
+
+previous_v1_params[266] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_diag,),
+    parser_y_modes=(ParseMode.proximity_diag,), max_reparse=0)
+
+previous_v1_params[270] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_normal,),
+    parser_y_modes=(ParseMode.color_proximity_diag,), max_reparse=1)
+
+previous_v1_params[271] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_normal,),
+    parser_y_modes=(ParseMode.proximity_normal,), max_reparse=0)
+
+previous_v1_params[275] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_diag,),
+    parser_y_modes=(ParseMode.proximity_diag,), max_reparse=0)
+
+previous_v1_params[276] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_diag,),
+    parser_y_modes=(ParseMode.proximity_diag,), max_reparse=0)
+
+previous_v1_params[289] = GlobalParams(
+    parser_x_modes=(ParseMode.color_proximity_diag,),
+    parser_y_modes=(ParseMode.color_proximity_diag,), max_reparse=0)
+
+previous_v1_params[290] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_diag,),
+    parser_y_modes=(ParseMode.crop,), max_reparse=0)
+
+previous_v1_params[294] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_diag,),
+    parser_y_modes=(ParseMode.crop,), max_reparse=0)
