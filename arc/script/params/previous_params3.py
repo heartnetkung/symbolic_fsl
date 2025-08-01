d6ef5a902b69d25727c160a5b58647565799f5f9
heartnetkung <heartnetkung@gmail.com>
2025-08-01 22:52:41 +0100

    add more previous params

--- arc/script/params/previous_params3.py
+++ arc/script/params/previous_params3.py
@@ -85,3 +85,11 @@ previous_v1_params3[294] = GlobalParams(
 previous_v1_params3[297] = GlobalParams(
     parser_x_modes=(ParseMode.proximity_diag,),
     parser_y_modes=(ParseMode.color_proximity_diag,), max_reparse=1)
+
+previous_v1_params3[298] = GlobalParams(
+    parser_x_modes=(ParseMode.color_proximity_diag,),
+    parser_y_modes=(ParseMode.color_proximity_diag,), max_reparse=1)
+
+previous_v1_params3[299] = GlobalParams(
+    parser_x_modes=(ParseMode.proximity_diag,),
+    parser_y_modes=(ParseMode.proximity_diag,), max_reparse=0)
--- arc/script/solve_previous.py
+++ arc/script/solve_previous.py
@@ -6,7 +6,7 @@ from .params.previous_params3 import previous_v1_params3
 from .params.previous_params4 import previous_v1_params4
 import traceback
 
-SKIP = {31, 297}
+SKIP = {31, 155, 297, 298}
 
 
 def solve_previous(index: int = -1)->None:
