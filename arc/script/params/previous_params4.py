5213046df077f3153409a3ac9714b6ac184d8a07
heartnetkung <heartnetkung@gmail.com>
2025-08-05 12:16:24 +0100

    add more previous params

--- arc/script/params/previous_params4.py
+++ arc/script/params/previous_params4.py
@@ -50,6 +50,14 @@ previous_v1_params4[338] = GlobalParams(
     parser_x_modes=(ParseMode.proximity_diag,),
     parser_y_modes=(ParseMode.proximity_diag,), max_reparse=0)
 
+previous_v1_params4[343] = GlobalParams(
+    parser_x_modes=(ParseMode.crop,),
+    parser_y_modes=(ParseMode.crop,), max_reparse=0)
+
+previous_v1_params4[346] = GlobalParams(
+    parser_x_modes=(ParseMode.crop,),
+    parser_y_modes=(ParseMode.crop,), max_reparse=0)
+
 previous_v1_params4[346] = GlobalParams(
     parser_x_modes=(ParseMode.crop,),
     parser_y_modes=(ParseMode.crop,), max_reparse=0)
