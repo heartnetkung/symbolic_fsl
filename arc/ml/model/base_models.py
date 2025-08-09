fix numerical bug in ml

arc/ml/model/base_models.py
@@ -37,9 +37,11 @@ class PolynomialClassifier(MLModel):
         return f'if {_poly_to_str(self.columns, coefs2)} < {coef0}:'
 
     def predict(self, X: pd.DataFrame)->np.ndarray:
+        result = np.matmul(make_cls_features(X, self.params), self.coefs)
+        is_equal, is_greater = np.isclose(result, 0), result > 0
         if self.is_greater_than:
-            return np.matmul(make_cls_features(X, self.params), self.coefs) >= 0
-        return np.matmul(make_cls_features(X, self.params), self.coefs) < 0
+            return np.logical_or(is_greater, is_equal)
+        return np.logical_not(np.logical_or(is_greater, is_equal))
 
 
 class EPDT(MLModel):

arc/ml/model/comparison_factory.py
@@ -19,8 +19,8 @@ class ComparisonModel(MLModel):
 
     def predict(self, X: pd.DataFrame)->np.ndarray:
         if self.eq:
-            return X[self.col1] == X[self.col2]
-        return X[self.col1] != X[self.col2]
+            return np.isclose(X[self.col1], X[self.col2])
+        return np.logical_not(np.isclose(X[self.col1], X[self.col2]))
 
     def _to_code(self) -> str:
         if self.eq:
@@ -42,8 +42,8 @@ class ConstantComparisonModel(MLModel):
 
     def predict(self, X: pd.DataFrame)->np.ndarray:
         if self.eq:
-            return X[self.col] == self.value
-        return X[self.col] != self.value
+            return np.isclose(X[self.col], self.value)
+        return np.logical_not(np.isclose(X[self.col], self.value))
 
     def _to_code(self) -> str:
         if self.eq:
@@ -64,11 +64,15 @@ def make_comparison_models(X: pd.DataFrame, y: np.ndarray,
         all_correct_values = set(X[col][y])
         if len(all_correct_values) == 1:
             value = all_correct_values.pop()
-            result.append(ConstantComparisonModel(col, value, True, params))
+            candidate = ConstantComparisonModel(col, value, True, params)
+            if np.array_equal(candidate.predict(X), y):
+                result.append(candidate)
 
         all_incorrect_values = set(X[col][np.logical_not(y)])
         if len(all_incorrect_values) == 1:
             value = all_incorrect_values.pop()
-            result.append(ConstantComparisonModel(col, value, False, params))
+            candidate = ConstantComparisonModel(col, value, False, params)
+            if np.array_equal(candidate.predict(X), y):
+                result.append(candidate)
 
     return result