6acb19f7139d06ed491f312ad02ec3ddfe500ddb
heartnetkung <heartnetkung@gmail.com>
2025-08-11 14:46:15 +0100

    add previous params

--- arc/script/params/previous_params4.py
+++ arc/script/params/previous_params4.py
@@ -58,6 +58,10 @@ previous_v1_params4[328] = proto.update(
     parser_x_modes=(ParseMode.crop,),
     parser_y_modes=(ParseMode.crop,), enable_free_draw=True)
 
+previous_v1_params4[329] = proto.update(
+    parser_x_modes=(ParseMode.color_proximity_diag,),
+    parser_y_modes=(ParseMode.color_proximity_diag,))
+
 previous_v1_params4[330] = proto.update(
     parser_x_modes=(ParseMode.crop,),
     parser_y_modes=(ParseMode.crop,), enable_free_draw=True)
