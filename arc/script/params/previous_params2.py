368e304c23ffc54671bd2b59d22c3707831f6d34
heartnetkung <heartnetkung@gmail.com>
2025-08-11 14:25:04 +0100

    solve 120

--- arc/script/params/previous_params2.py
+++ arc/script/params/previous_params2.py
@@ -38,6 +38,10 @@ previous_v1_params2[116] = proto.update(
     parser_x_modes=(ParseMode.proximity_diag,),
     parser_y_modes=(ParseMode.proximity_diag,))
 
+previous_v1_params2[120] = proto.update(
+    parser_x_modes=(ParseMode.proximity_diag,),
+    parser_y_modes=(ParseMode.proximity_diag,))
+
 previous_v1_params2[124] = proto.update(
     parser_x_modes=(ParseMode.crop,),
     parser_y_modes=(ParseMode.crop,))
