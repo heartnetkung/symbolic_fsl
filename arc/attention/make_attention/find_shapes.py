cc52f239ee5b38603d3c3f6cd2ca70f4f8dded2f
heartnetkung <heartnetkung@gmail.com>
2025-08-13 18:29:17 +0100

    implement new find_shapes

--- arc/attention/make_attention/find_shapes.py
+++ arc/attention/make_attention/find_shapes.py
@@ -1,14 +1,50 @@
 from ...graphic import *
-from ..low_level import *
 from typing import Optional
-from ...ml import *
 import pandas as pd
+from copy import deepcopy
 
 
 def find_common_y_shapes(all_y_shapes: list[list[Shape]])->list[Shape]:
-    '''Find consistent shapes across all samples.'''
-    props = gen_prop(all_y_shapes, False)
-    common_shapes = props.groupby('prop').aggregate('first').drop_duplicates()
-    common_shapes.sort_values(['sample_id', 'index'], inplace=True)  # type:ignore
-    return [all_y_shapes[id1][id2]
-            for id1, id2 in zip(common_shapes['sample_id'], common_shapes['index'])]
+    return _find_common_shapes(all_y_shapes)
+
+
+def _find_common_shapes(all_y_shapes: list[list[Shape]])->list[Shape]:
+    stats = {'sample_id': [], 'repr_': []}
+    shape_dict = {}
+
+    for sample, y_shapes in enumerate(all_y_shapes):
+        for index, y_shape in enumerate(y_shapes):
+            shape_repr = _to_repr(y_shape._grid)
+            if shape_repr is None:
+                continue
+
+            stats['sample_id'].append(sample)
+            stats['repr_'].append(shape_repr)
+            shape_dict[shape_repr] = _reset_position(y_shape)
+
+    stats_df = pd.DataFrame(stats)
+    grouped_df = stats_df.groupby('repr_').agg({'sample_id': 'nunique'})
+    filtered_df = grouped_df[grouped_df['sample_id'] > 1]
+    assert isinstance(filtered_df, pd.DataFrame)
+
+    # too many repeated shapes, include everything
+    if len(filtered_df) >= 3:
+        return list(shape_dict.values())
+
+    return [shape_dict[key] for key in filtered_df.index]
+
+
+def _to_repr(grid: Grid)->Optional[str]:
+    if grid.width*grid.height <= 2:
+        return None
+    return repr(grid)
+
+
+def _reset_position(shape: Shape)->Shape:
+    if shape.x == shape.y == 0:
+        return shape
+
+    result = deepcopy(shape)
+    result.x = 0
+    result.y = 0
+    return result
--- test/test_attention/test_find_shapes.py
+++ test/test_attention/test_find_shapes.py
@@ -3,34 +3,197 @@ from ...arc.graphic import *
 from ...arc.attention.low_level import *
 from ...arc.attention.make_attention.find_shapes import *
 
-shape1 = Unknown(0, 0, Grid([[-1, 1, -1], [1, 1, 1], [-1, 1, -1]]))
-shape2 = FilledRectangle(1, 1, 1, 1, 1)
-shape3 = FilledRectangle(1, 1, 1, 1, 2)
-shape4 = FilledRectangle(2, 2, 2, 2, 2)
-shape5 = FilledRectangle(3, 3, 3, 3, 3)
-shape6 = FilledRectangle(6, 6, 6, 6, 6)
 
-
-def test_common_y_shapes():
-    result = find_common_y_shapes([[shape1, shape2, shape5], [shape1, shape3, shape4]])
-    assert result == [shape1, shape2]
-
-
-def test_common_y_shapes2():
-    result = find_common_y_shapes([[shape1, shape2, shape5], [shape4, shape6]])
-    assert result == []
-
-
-def test_common_y_shapes3():
-    problem_no = 11
-    ds = read_datasets(DatasetChoice.train_v1)[problem_no]
-    all_y_shapes = [list_sparse_objects(y_grid.remove_bg(), True)
-                    for y_grid in ds.y_train]
+def test_281():
+    all_y_shapes = [
+        list_sparse_objects(y281_0.replace_color(0, NULL_COLOR)),
+        list_sparse_objects(y281_1.replace_color(0, NULL_COLOR))
+    ]
     result = find_common_y_shapes(all_y_shapes)
-    assert len(result) == 1
-    assert result[0] == Unknown(0, 1, Grid([
-        [2, -1, 7, -1, 2],
-        [-1, 2, 7, 2, -1],
-        [7, 7, 2, 7, 7],
-        [-1, 2, 7, 2, -1],
-        [2, -1, 7, -1, 2]]))
+    assert result == [Unknown(0, 0, Grid([[5, 1, 5], [1, -1, 1], [5, 1, 5]]))]
+
+
+def test_333():
+    all_y_shapes = [
+        [Unknown(0, 0, y333_0)],
+        [Unknown(0, 0, y333_1)],
+        [Unknown(0, 0, y333_2)],
+        [Unknown(0, 0, y333_3)],
+        [Unknown(0, 0, y333_4)],
+        [Unknown(0, 0, y333_5)],
+        [Unknown(0, 0, y333_6)]
+    ]
+    result = find_common_y_shapes(all_y_shapes)
+    assert set(result) == {shapes[0] for shapes in all_y_shapes}
+
+
+def test_370():
+
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
+    all_y_shapes = [
+        list_sparse_objects(y370_0.replace_color(0, NULL_COLOR)),
+        list_sparse_objects(y370_1.replace_color(0, NULL_COLOR))
+    ]
+    result = find_common_y_shapes(all_y_shapes)
+    expected = {Unknown(0, 0, Grid([[-1, 3, -1], [3, 3, 3], [-1, 3, -1]]))}
+    assert set(result) == expected
+
+
+def test_398():
+    all_y_shapes = [
+        [Unknown(0, 0, y398_0.replace_color(0, NULL_COLOR)),
+         Unknown(0, 0, y398_0.replace_color(0, NULL_COLOR))],
+        [Unknown(0, 0, y398_1.replace_color(0, NULL_COLOR))],
+        [Unknown(0, 0, y398_2.replace_color(0, NULL_COLOR))],
+        [Unknown(0, 0, y398_3.replace_color(0, NULL_COLOR))],
+        [Unknown(0, 0, y398_4.replace_color(0, NULL_COLOR))],
+        [Unknown(0, 0, y398_5.replace_color(0, NULL_COLOR))],
+        [Unknown(0, 0, y398_6.replace_color(0, NULL_COLOR))],
+        [Unknown(0, 0, y398_7.replace_color(0, NULL_COLOR))]
+    ]
+    result = find_common_y_shapes(all_y_shapes)
+    assert set(result) == {shapes[0] for shapes in all_y_shapes}
+
+
+y281_0 = Grid([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 5, 1, 5, 0, 0, 0, 0],
+    [0, 0, 1, 0, 1, 0, 0, 0, 0],
+    [0, 0, 5, 1, 5, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 5, 1, 5, 0],
+    [0, 0, 0, 0, 0, 1, 0, 1, 0],
+    [0, 5, 1, 5, 0, 5, 1, 5, 0],
+    [0, 1, 0, 1, 0, 0, 0, 0, 0],
+    [0, 5, 1, 5, 0, 0, 0, 0, 0]
+])
+
+y281_1 = Grid([
+    [0, 5, 1, 5, 0, 0, 5, 1, 5],
+    [0, 1, 0, 1, 0, 0, 1, 0, 1],
+    [0, 5, 1, 5, 0, 0, 5, 1, 5],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 5, 1, 5, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 0, 0, 0, 0],
+    [0, 5, 1, 5, 0, 5, 1, 5, 0],
+    [0, 0, 0, 0, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 5, 1, 5, 0]
+])
+
+y333_0 = Grid([
+    [5, 5, 5],
+    [0, 5, 0],
+    [0, 5, 0]
+])
+
+y333_1 = Grid([
+    [0, 5, 0],
+    [5, 5, 5],
+    [0, 5, 0]
+])
+
+y333_2 = Grid([
+    [0, 0, 5],
+    [0, 0, 5],
+    [5, 5, 5]
+])
+
+y333_3 = Grid([
+    [0, 5, 0],
+    [5, 5, 5],
+    [0, 5, 0]
+])
+
+y333_4 = Grid([
+    [5, 5, 5],
+    [0, 5, 0],
+    [0, 5, 0]
+])
+
+y333_5 = Grid([
+    [5, 5, 5],
+    [0, 5, 0],
+    [0, 5, 0]
+])
+
+y333_6 = Grid([
+    [0, 0, 5],
+    [0, 0, 5],
+    [5, 5, 5]
+])
+
+y370_0 = Grid([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0, 0, 0],
+    [0, 1, 0, 3, 3, 3, 0, 1, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+])
+
+y370_1 = Grid([
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0, 0, 0],
+    [0, 0, 3, 3, 3, 0, 0, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+])
+
+y398_0 = Grid([
+    [1, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+])
+
+y398_1 = Grid([
+    [1, 0, 1],
+    [0, 0, 0],
+    [0, 0, 0]
+])
+
+y398_2 = Grid([
+    [1, 0, 1],
+    [0, 1, 0],
+    [0, 0, 0]
+])
+
+y398_3 = Grid([
+    [1, 0, 1],
+    [0, 0, 0],
+    [0, 0, 0]
+])
+
+y398_4 = Grid([
+    [1, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+])
+
+y398_5 = Grid([
+    [1, 0, 1],
+    [0, 1, 0],
+    [1, 0, 0]
+])
+
+y398_6 = Grid([
+    [1, 0, 1],
+    [0, 1, 0],
+    [1, 0, 1]
+])
+
+y398_7 = Grid([
+    [1, 0, 1],
+    [0, 1, 0],
+    [1, 0, 0]
+])
