2f587e960c478ee530fc9cafe3fdcb86fc7675e5
heartnetkung <heartnetkung@gmail.com>
2025-08-17 01:01:41 +0100

    try preventing bad attention

--- arc/attention/make_attention/cluster_y.py
+++ arc/attention/make_attention/cluster_y.py
@@ -6,14 +6,14 @@ import numpy as np
 from sklearn.cluster import OPTICS, DBSCAN
 
 
-def cluster_y(rel_df: pd.DataFrame)->list[pd.DataFrame]:
+def cluster_y(rel_df: pd.DataFrame, sample_count: int)->list[pd.DataFrame]:
     '''
     Cluster y shapes based on their consistent relationship with x.
     For a cluster to be valid, it must be consistent and surjective.
     The result is all possible cluster formation.
     '''
     y_df, feat_arr = _preprocess(rel_df)
-    return _cluster_y(y_df, feat_arr)
+    return _cluster_y(y_df, feat_arr, sample_count)
 
 
 def _preprocess(rel_df: pd.DataFrame)->tuple[pd.DataFrame, pd.DataFrame]:
@@ -21,8 +21,9 @@ def _preprocess(rel_df: pd.DataFrame)->tuple[pd.DataFrame, pd.DataFrame]:
     return y_df, y_df.iloc[:, 2:]
 
 
-def _cluster_y(y_df: pd.DataFrame, feat_arr: pd.DataFrame)->list[pd.DataFrame]:
-    min_samples = cal_consistency_requirement(y_df)
+def _cluster_y(y_df: pd.DataFrame, feat_arr: pd.DataFrame,
+               sample_count: int)->list[pd.DataFrame]:
+    min_samples = min(sample_count, 3)
     models = [OPTICS(min_samples=min_samples, metric='precomputed', max_eps=99),
               DBSCAN(min_samples=min_samples, metric='precomputed')]
     metrics = ['l1', 'braycurtis']
