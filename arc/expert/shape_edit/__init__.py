08e1316d3a0dded21843dba988521bfae84a9ad9
heartnetkung <heartnetkung@gmail.com>
2025-09-10 11:08:02 +0100

    remove fritb

--- arc/expert/shape_edit/__init__.py
+++ arc/expert/shape_edit/__init__.py
@@ -1,4 +1,3 @@
 from .fitb_expert import ExpansionMode, FillInTheBlank, FillInTheBlankExpert
 from .hamming_expert import Hamming, HammingExpert
 from .fitp_expert import FITP, FITPExpert
-from .fritb import FRITBCondition, FillRectangleInTheBlank
