cb7b2eaa5eaf60a22c0e49e28b632d5a628c01a5
heartnetkung <heartnetkung@gmail.com>
2025-10-21 23:48:08 +0100

    fix 221

--- arc/script/params/previous_params3.py
+++ arc/script/params/previous_params3.py
@@ -24,7 +24,7 @@ previous_v1_params3[219] = proto.update(
 
 previous_v1_params3[221] = proto.update(
     parser_x_modes=(ParseMode.color_proximity_diag,),
-    parser_y_modes=(ParseMode.proximity_diag,))
+    parser_y_modes=(ParseMode.proximity_diag,), enable_stack=True)
 
 previous_v1_params3[222] = proto.update(
     parser_x_modes=(ParseMode.proximity_diag,),
